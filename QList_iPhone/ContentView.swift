6594d7f4fadc385a60d255d90be93e92f075c07d
durban.zhang <durban.zhang@gmail.com>
2020-08-23 21:55:20 +0800

    Initial Commit

---
+++ QList_iPhone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QList_iPhone
+//
+//  Created by durban.zhang on 2020/8/23.
+//  Copyright © 2020 durban.zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
