2375e33995782b760c62627f6dcf60d940bdd2c5
durban.zhang <durban.zhang@gmail.com>
2020-09-03 23:33:23 +0800

    add symbol line

---
+++ QList_iPhone/Views/BadgeSymbol.swift
@@ -0,0 +1,38 @@
+//
+//  BadgeSymbol.swift
+//  QList_iPhone
+//
+//  Created by durban.zhang on 2020/9/2.
+//  Copyright © 2020 durban.zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width / 2
+                let topWidth = width * 0.226
+                let topHeight = height * 0.448
+                
+                path.addLines([
+                    CGPoint(x: width, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+            }
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
