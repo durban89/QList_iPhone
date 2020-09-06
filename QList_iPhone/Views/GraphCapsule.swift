e720b76a64c4c403853bd553a1a9bccf711a8498
durban.zhang <durban.zhang@gmail.com>
2020-09-06 18:59:22 +0800

    add GraphCapsule

--- QList_iPhone/Views/GraphCapsule.swift
+++ QList_iPhone/Views/GraphCapsule.swift
@@ -9,13 +9,29 @@
 import SwiftUI
 
 struct GraphCapsule: View {
+    var index: Int
+    var height: CGFloat
+    var range: Range<Double>
+    var overallRange: Range<Double>
+    
+    var heightRatio: CGFloat {
+        max(CGFloat(magnitude(of: range) / magnitude(of: overallRange)), 0.15)
+    }
+    
+    var offsetRatio: CGFloat {
+        CGFloat((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Capsule()
+            .fill(Color.white)
+            .frame(height: height * heightRatio)
+            .offset(x: 0, y: height * -offsetRatio)
     }
 }
 
 struct GraphCapsule_Previews: PreviewProvider {
     static var previews: some View {
-        GraphCapsule()
+        GraphCapsule(index: 0, height: 150, range: 10..<50, overallRange: 0..<100)
     }
 }
