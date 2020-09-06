95a3469bfbce93e35cc95b44ea425aea02121ab5
durban.zhang <durban.zhang@gmail.com>
2020-09-06 19:00:33 +0800

    add userdata environmentObject

--- QList_iPhone/LandmarkList.swift
+++ QList_iPhone/LandmarkList.swift
@@ -37,5 +37,6 @@ struct LandmarkList_Previews: PreviewProvider {
             LandmarkList().previewDevice(PreviewDevice(rawValue: deviceName))
             .previewDisplayName(deviceName)
         }
+        .environmentObject(UserData())
     }
 }
