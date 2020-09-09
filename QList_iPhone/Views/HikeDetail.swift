053bd90ae846190fc554e7f85e1f748ad17e1d9e
durban.zhang <durban.zhang@gmail.com>
2020-09-09 22:27:52 +0800

    update HikeDetail

--- QList_iPhone/Views/HikeDetail.swift
+++ QList_iPhone/Views/HikeDetail.swift
@@ -10,9 +10,30 @@ import SwiftUI
 
 struct HikeDetail: View {
     var hike: Hike
+    @State var dataToShow = \Hike.Observation.elevation
     
+    var buttons = [
+        ("Elevation", \Hike.Observation.elevation),
+        ("HeartRate", \Hike.Observation.heartRate),
+        ("Pace", \Hike.Observation.pace),
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HikeGraph(hike: hike, path: dataToShow)
+                .frame(height: 200)
+            HStack{
+                ForEach(buttons, id: \.0) { button in
+                    Button(action: {
+                        self.dataToShow = button.1
+                    }) {
+                        Text(button.0)
+                            .foregroundColor(button.1 == self.dataToShow ? Color.gray : Color.accentColor)
+                            .font(.system(size: 15))
+                            .animation(nil)
+                    }
+                }
+            }
+        }
     }
 }
 
