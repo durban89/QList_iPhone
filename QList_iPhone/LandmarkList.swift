6965cde62d08c660b90b065ae0a547197042da96
durban.zhang <durban.zhang@gmail.com>
2020-09-18 22:37:14 +0800

    complete landmark

--- QList_iPhone/LandmarkList.swift
+++ QList_iPhone/LandmarkList.swift
@@ -12,31 +12,29 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List{
-                Toggle(isOn: $userData.showFavoriteOnly) {
-                    Text("Favorite")
-                }
-                
-                ForEach(userData.landmarks) {landmark in
-                    if !self.userData.showFavoriteOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)){
-                            LandmarkRow(landmark: landmark)
-                        }
+        List{
+            Toggle(isOn: $userData.showFavoriteOnly) {
+                Text("Favorite")
+            }
+            
+            ForEach(userData.landmarks) {landmark in
+                if !self.userData.showFavoriteOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle(Text("Landmarks"))
         }
+        .navigationBarTitle(Text("Landmarks"))
+    
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList().previewDevice(PreviewDevice(rawValue: deviceName))
-            .previewDisplayName(deviceName)
+        NavigationView {
+            LandmarkList()
+            .environmentObject(UserData())
         }
-        .environmentObject(UserData())
     }
 }
