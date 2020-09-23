d8adf90a0ddf18b9c3fb8941467426440b2a296c
durban.zhang <durban.zhang@gmail.com>
2020-09-23 22:43:19 +0800

    update profile summary

--- QList_iPhone/Views/Profile/ProfileSummary.swift
+++ QList_iPhone/Views/Profile/ProfileSummary.swift
@@ -30,6 +30,21 @@ struct ProfileSummary: View {
             Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
             
             Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+            
+            VStack{
+                Text("Completed Badges")
+                ScrollView{
+                    HStack{
+                        HikeBadge(name: "First Hike")
+                        HikeBadge(name: "Earth Day")
+                            .hueRotation(Angle(degrees: 90))
+                        HikeBadge(name: "Tenth Hike")
+                            .grayscale(0.5)
+                            .hueRotation(Angle(degrees: 45))
+                    }
+                }
+            }
+            .frame(height: 140)
         }
     }
 }
