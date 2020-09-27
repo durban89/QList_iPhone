be3fa54fa49501461b183a21b9f8219de72f548d
durban.zhang <durban.zhang@gmail.com>
2020-09-27 22:15:18 +0800

    add ProfileEditor

---
+++ QList_iPhone/Views/Profile/ProfileEditor.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileEditor.swift
+//  QList_iPhone
+//
+//  Created by durban.zhang on 2020/9/27.
+//  Copyright © 2020 durban.zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    
+    var body: some View {
+        List{
+            HStack{
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+            
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
