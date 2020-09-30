c3b6c60bae9764e534db3ffa8a17d3fa7a44f8d7
durban.zhang <durban.zhang@gmail.com>
2020-09-30 23:25:16 +0800

    add datepicker

--- QList_iPhone/Views/Profile/ProfileEditor.swift
+++ QList_iPhone/Views/Profile/ProfileEditor.swift
@@ -11,6 +11,11 @@ import SwiftUI
 struct ProfileEditor: View {
     @Binding var profile: Profile
     
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
     
     var body: some View {
         List{
@@ -33,6 +38,17 @@ struct ProfileEditor: View {
                     }
                 }
                 .pickerStyle(SegmentedPickerStyle())
+            }
+            .padding(.top)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Goal Date").bold()
+                
+                DatePicker(
+                    "Goal Date",
+                    selection: $profile.goalDate,
+                    in: dateRange,
+                    displayedComponents: .date)
             }
             .padding(.top)
         }
