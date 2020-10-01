d262338b6253594e52776b3d7f371db6f9ef78ae
durban.zhang <durban.zhang@gmail.com>
2020-10-01 22:31:37 +0800

    add cancel button

--- QList_iPhone/Views/Profile/ProfileHost.swift
+++ QList_iPhone/Views/Profile/ProfileHost.swift
@@ -16,6 +16,12 @@ struct ProfileHost: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack{
+                if (self.mode?.wrappedValue == .active) {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 
                 EditButton()
