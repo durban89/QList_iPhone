f98377a5eb2a4e806fd4cca2763667214b97432f
durban.zhang <durban.zhang@gmail.com>
2020-10-04 22:16:16 +0800

    add PageView

---
+++ QList_iPhone/Views/PageView.swift
@@ -0,0 +1,46 @@
+//
+//  PageView.swift
+//  QList_iPhone
+//
+//  Created by durban.zhang on 2020/10/4.
+//  Copyright © 2020 durban.zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+            .overlay(TextOverlay(landmark: landmark))
+    }
+}
+
+struct TextOverlay: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        Text("TextOverlay")
+    }
+}
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+    
+    
+    var body: some View {
+        PageViewController(controllers: viewControllers)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(features.map { FeatureCard })
+    }
+}
