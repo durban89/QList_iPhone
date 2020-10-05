f6da6c2472392cee450af785781716f9267b412a
durban.zhang <durban.zhang@gmail.com>
2020-10-05 23:14:35 +0800

    update Coordinator

--- QList_iPhone/Views/PageViewController.swift
+++ QList_iPhone/Views/PageViewController.swift
@@ -28,11 +28,35 @@ struct PageViewController: UIViewControllerRepresentable {
         pageViewController.setViewControllers([controllers[0]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
         var parent: PageViewController
         
         init(_ pageViewController: PageViewController) {
             self.parent = pageViewController
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            
+            return parent.controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index == 0 {
+                return parent.controllers.last
+            }
+            
+            return parent.controllers[index - 1]
         }
     }
 }
