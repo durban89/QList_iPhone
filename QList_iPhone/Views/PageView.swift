8a87360ff87ab5f217a6788045ce279db804c3e9
durban.zhang <durban.zhang@gmail.com>
2020-10-10 23:58:19 +0800

    user UIHostingController - 不过会导致一个问题就是 Canvas 中不能使用preview

--- QList_iPhone/Views/PageView.swift
+++ QList_iPhone/Views/PageView.swift
@@ -8,42 +8,26 @@
 
 import SwiftUI
 
-struct FeatureCard: View {
-    var landmark: Landmark
-    var body: some View {
-        landmark.featureImage?
-            .resizable()
-            .aspectRatio(3 / 2, contentMode: .fit)
-            .overlay(TextOverlay(landmark: landmark))
-    }
-}
-
-struct TextOverlay: View {
-    var landmark: Landmark
-    
-    var body: some View {
-        Text("TextOverlay")
-    }
-}
-
 struct PageView<Page: View>: View {
     var viewControllers: [UIHostingController<Page>]
+    
     @State var currentPage = 0
     
     init(_ views: [Page]) {
-        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+        self.viewControllers = views.map {
+            UIHostingController(rootView: $0)
+        }
     }
-    
+
     
     var body: some View {
         PageViewController(controllers: viewControllers, currentPage: $currentPage)
-        Text("Current Page \(currentPage)")
     }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView(features.map { FeatureCard(landmark: $0) })
+        PageView([FeatureCard(landmark: landmarkData[0])])
             .aspectRatio(3 / 2, contentMode: .fit)
             
     }
